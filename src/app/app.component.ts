5ef9aeb2a4c2c6d5569867ac888f6f90c2155e1c
GunA123-seelan <gunaseelanmsc123@gmail.com>
2025-04-08 10:42:22 +0530

    listAr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  list=['Front End Developer','Angular Developer'];
+  display='';
+  currentWord:any;
+  val:number=0;
+  letterIndex:number=0;
+
+  ngOnInit(): void {
+    this.loadAr();
+  }
+
+  loadAr=()=>{
+    this.currentWord=this.list[this.val];
+    this.display='';
+    this.letterIndex=0;
+
+    const interval=setInterval(()=>{
+      if(this.letterIndex<this.currentWord.length){
+        this.display+=this.currentWord[this.letterIndex];
+        this.letterIndex++;      
+      }else{
+        clearInterval(interval);
+        setTimeout(()=>{
+          this.val=(this.val+1) % this.list.length;
+          this.loadAr();
+        },1000)
+      }
+    },300)
+
+  }
 }
